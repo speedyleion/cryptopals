39a771391eeccc3291743eda17de858f7bfad0a4
Nick <speedyleion@users.noreply.github.com>
2023-03-20 19:54:59 -0700

    increased loading time by a bit
    base64 decode working now

--- src/ecb_crack.rs
+++ src/ecb_crack.rs
@@ -1,11 +1,17 @@
 use rand::{Rng, SeedableRng};
-use crate::{BLOCK_SIZE, ecb, EncryptionMode, pkcs, random_prefix_and_suffix};
+use rand_chacha::ChaChaRng;
+use crate::{BLOCK_SIZE, ecb, pkcs, random_prefix_and_suffix};
 
 fn fixed_ecb_encryptor(input: impl AsRef<[u8]>) -> Vec<u8> {
-    let mut rng = rand::from_seed();
+    let mut rng = ChaChaRng::seed_from_u64(0);
     let key = Rng::gen::<[u8; BLOCK_SIZE]>(&mut rng);
     let input = pkcs::pad(random_prefix_and_suffix(input), BLOCK_SIZE as u8);
-        ecb::encrypt(key.as_ref(), input)
+    ecb::encrypt(key.as_ref(), input)
+}
+
+fn crack_ecb(encrypted: impl AsRef<[u8]>) -> Vec<u8> {
+    vec![]
+
 }
 
 #[cfg(test)]
@@ -14,15 +20,15 @@ mod test {
     use base64::Engine;
 
     #[test]
-    fn detect_ecb() {
-        let input = r#"
-            Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
-            aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
-            dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
-            YnkK"#;
-        let bytes = base64::engine::general_purpose::STANDARD
-            .decode(textwrap::dedent(input).trim())
+    fn successfull_crack_of_ecb() {
+
+        let input = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
+        let bytes = base64::engine::general_purpose::STANDARD_NO_PAD
+            .decode(input)
             .unwrap();
         let encrypted = fixed_ecb_encryptor(bytes);
+
+        let cracked = crack_ecb(&encrypted);
+        assert_eq!(String::from_utf8(cracked).unwrap(), "what")
     }
 }
